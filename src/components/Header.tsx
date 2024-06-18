5249acf402e9d98f50cf9731cd95c5d2cad2aabf
ohgus <123801385+ohgus@users.noreply.github.com>
2024-06-18 20:02:00 +0900

    [refactor]: 헤더에 홈 이동 버튼 추가 (#3)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header className="flex justify-between mt-5">
+      <Link href="/">Home</Link>
       <h1 className="font-bold text-lg">Share The Journey</h1>
       <Link href="/login">Login</Link>
     </header>
