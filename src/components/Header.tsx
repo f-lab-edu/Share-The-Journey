acc5c7ce4a5081db63cdc9e702eafa281d0626a1
ohgus <123801385+ohgus@users.noreply.github.com>
2024-07-03 23:17:15 +0900

    [feat]: 로그인 여부에 따른 추가 기능 구현 (#18) - 로그인한 경우 로그아웃 버튼을 보여준다. - 로그아웃 핸들러 구현.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,37 @@
+'use client';
+
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import auth from '@/app/auth';
 
 const Header = () => {
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, []);
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        router.push('/');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <header className="flex justify-between mt-5">
       <Link href="/">Home</Link>
       <h1 className="font-bold text-lg">Share The Journey</h1>
-      <Link href="/login">Login</Link>
+      {user && <button onClick={handleLogout}>Logout</button>}
+      {!user && <Link href="/login">Login</Link>}
     </header>
   );
 };
