0ec5a392063dcc2466f281684fc2f3cdf3d6d1e6
ohgus <123801385+ohgus@users.noreply.github.com>
2024-07-04 00:51:17 +0900

    [feat]: 회원가입 구현 (#18)

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,6 +1,29 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import auth from '@/app/auth';
+
 import Header from '@/components/Header';
 
 const Page = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        router.push('/');
+      })
+      .catch((err) => {
+        alert('회원가입에 실패했습니다.');
+        console.error(err);
+      });
+  };
+
   return (
     <>
       <Header />
@@ -8,13 +31,15 @@ const Page = () => {
         Sign Up
       </h1>
       <div className="w-2/5 mx-auto rounded-lg bg-slate-100 p-4">
-        <form className="mt-1">
+        <form className="mt-1" onSubmit={handleSubmit}>
           <label className="block">
             <h3 className="font-semibold">이메일</h3>
             <input
               className="my-2 rounded-md w-full p-1 px-2"
               type="email"
               name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </label>
           <label className="block">
@@ -23,16 +48,18 @@ const Page = () => {
               className="rounded-md w-full my-2 p-1 px-2"
               type="password"
               name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </label>
-          <label className="block">
+          {/* <label className="block">
             <h3 className="font-semibold">닉네임</h3>
             <input
               className="rounded-md w-full my-2 p-1 px-2"
               type="text"
               name="nickname"
             />
-          </label>
+          </label> */}
           <button
             className="block bg-green-600 rounded-2xl p-2 w-full text-white font-semibold my-2"
             type="submit"
