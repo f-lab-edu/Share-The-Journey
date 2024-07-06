343c4f0dacb2a11cf731883b20e6f46da323b441
ohgus <123801385+ohgus@users.noreply.github.com>
2024-07-06 17:42:58 +0900

    [refactor]: 검색 기능을 위한 추가 작업 (#17) - 검색어를 입력하면 /search 페이지로 이동 - 검색어를 쿼리파라미터로 추가

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,32 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const router = useRouter();
+
+  const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (searchQuery.trim() === '') return;
+    router.push(`/search?query=${encodeURIComponent(searchQuery)}`);
+  };
+
   return (
-    <section className="flex justify-center w-8/12 mx-auto">
-      <input
-        type="search"
-        placeholder="여행지를 검색해보세요."
-        className="w-3/4 border rounded-3xl border-slate-300 px-3 py-2"
-      />
+    <section className="w-8/12 mx-auto">
+      <form
+        onSubmit={handleSearchSubmit}
+        className="w-full flex justify-center"
+      >
+        <input
+          type="search"
+          placeholder="여행지를 검색해보세요."
+          className="w-3/4 border rounded-3xl border-slate-300 px-3 py-2 mx-auto"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </form>
     </section>
   );
 };
