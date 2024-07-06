7e6261bb14bdaa858d85398051ff25df990f6ad4
ohgus <123801385+ohgus@users.noreply.github.com>
2024-07-06 18:07:28 +0900

    [feat]: 검색 페이지에 검색 api 적용 (#17) - 검색 쿼리에 맞는 데이터를 가져와 리스트를 보여준다 - 쿼리를 페이지 타이틀에 적용 - 검색 결과가 없는 상황에 맞는 메세지를 출력

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,8 @@
-import placeData from '../places.json';
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import currency from 'currency.js';
@@ -9,26 +13,70 @@ import SearchBar from '@/components/SearchBar';
 import { PlaceCardProps } from '@/types/place';
 
 const Page = () => {
+  const [searchResult, setSearchResult] = useState<PlaceCardProps[]>([]);
+  const searchParams = useSearchParams();
+  const query = searchParams.get('query');
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchSearchResults = async () => {
+      try {
+        const response = await fetch('/api/search', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ query }),
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch search results');
+        }
+
+        const data = await response.json();
+        setSearchResult(data.data);
+      } catch (error) {
+        console.error('Search error:', error);
+      }
+    };
+
+    fetchSearchResults();
+  }, [query]);
+
   return (
     <>
       <Header />
-      <h1 className="text-center text-2xl font-bold my-10">검색 결과 보기</h1>
+      <h1 className="text-center text-2xl font-bold my-10">
+        {query} 검색 결과 보기
+      </h1>
       <SearchBar />
       <section className="w-8/12 mx-auto mt-20">
-        {placeData.map((place) => (
+        {searchResult &&
+          searchResult.map((place) => (
+            <>
+              <SearchResultCard
+                imgUrl={place.imgUrl}
+                name={place.name}
+                location={place.location}
+                price={place.price}
+                score={place.score}
+                id={place.id}
+                key={place.id}
+              />
+              <hr className="w-10/12 mx-auto" />
+            </>
+          ))}
+        {searchResult.length === 0 && (
           <>
-            <SearchResultCard
-              imgUrl={place.imgUrl}
-              name={place.name}
-              location={place.location}
-              price={place.price}
-              score={place.score}
-              id={place.id}
-              key={place.id}
-            />
-            <hr className="w-10/12 mx-auto" />
+            <p className="text-2xl text-center text-slate-400 font-semibold">
+              검색 결과를 찾을 수 없습니다.
+            </p>
+            <p className="text-center text-slate-400 mt-2">
+              다른 검색어로 다시 시도해주세요.
+            </p>
           </>
-        ))}
+        )}
       </section>
     </>
   );
