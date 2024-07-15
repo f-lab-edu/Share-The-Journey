a1b2f63f4b5f2a77b77f73e2260de47e06fcc917
ohgus <123801385+ohgus@users.noreply.github.com>
2024-07-15 17:58:35 +0900

    [refactor]: ReviewArea 컴포넌트 분리 (#23)

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,26 +1,13 @@
 import Image from 'next/image';
 import currency from 'currency.js';
-import { format, parse } from 'date-fns';
+
 import { doc, getDoc } from 'firebase/firestore';
 
-import reviewInfo from '../../reviews.json';
-
 import Header from '@/components/Header';
+import ReviewArea from '@/components/ReviewArea';
 import db from '@/app/db';
 
 import { PlaceDetailProps } from '@/types/place';
-
-type Review = {
-  description: string;
-  writer: string;
-  score: number;
-  date: string;
-  review_id: number;
-};
-
-type ReviewProps = {
-  reviews: Review[];
-};
 
 const fetchPlace = async (id: string): Promise<PlaceDetailProps | null> => {
   const docRef = doc(db, 'places', id);
@@ -45,10 +32,6 @@ const Page = async ({ params }: { params: { id: string } }) => {
     );
   }
 
-  // 리뷰는 임시로 고정된 데이터 사용
-  const reviewList: Review[] = reviewInfo.filter(
-    (review) => review.place_id === 1
-  )[0].reviews;
   const imgUrlStr = place.imgUrl ? place.imgUrl : '/default.png';
 
   return (
@@ -62,7 +45,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
         className="!h-[400px] !w-[700px] mx-auto my-20 rounded-lg"
       />
       <PlaceInfo {...place} />
-      <ReviewArea reviews={reviewList} />
+      <ReviewArea placeId={params.id} />
     </>
   );
 };
@@ -101,45 +84,6 @@ const PlaceInfo = ({
       </p>
       <p className="mb-3">편의시설: {amenitiesText}</p>
       <p className=" text-lg font-bold">한줄평: {review}</p>
-    </section>
-  );
-};
-
-const ReviewArea = ({ reviews }: ReviewProps) => {
-  return (
-    <section className="w-9/12 mx-auto  bg-gray-300 p-3 rounded-md mb-10">
-      <h2 className="text-xl font-extrabold mb-5">리뷰</h2>
-      {reviews.length > 0 ? (
-        reviews.map((review) => {
-          const parsedDate = parse(review.date, 'yyyy-MM-dd', new Date());
-          const formattedDate = format(parsedDate, 'yyyy.MM.dd');
-
-          return (
-            <div
-              key={review.review_id}
-              className="mb-5 border p-3 rounded-md bg-white"
-            >
-              <div className="flex justify-between">
-                <h3 className="text-lg font-bold">작성자: {review.writer}</h3>
-                <p className="font-semibold mb-1">
-                  별점: <span className="text-amber-400">{review.score}</span>
-                </p>
-              </div>
-              <p className="mb-1">{review.description}</p>
-              <p className="font-semibold text-zinc-300">
-                작성일: {formattedDate}
-              </p>
-            </div>
-          );
-        })
-      ) : (
-        <p className="mb-5 border p-2 rounded-md bg-white">리뷰가 없습니다.</p>
-      )}
-      <input
-        type="text"
-        placeholder="리뷰를 작성해주세요."
-        className="w-full border p-2 rounded-md h-[100px]"
-      />
     </section>
   );
 };
