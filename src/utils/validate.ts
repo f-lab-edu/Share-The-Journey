12015cc3834c19f2e0e3f769d01266fcf09fb56a
ohgus <123801385+ohgus@users.noreply.github.com>
2024-07-28 15:58:22 +0900

    [refactor]: validate 로직 추출 (#21)

---
+++ src/utils/validate.ts
@@ -0,0 +1,21 @@
+import { NewPlaceForm } from '@/types/place';
+
+export const validateNewPlaceForm = (props: NewPlaceForm) => {
+  const { name, location, price, score, review, amenities } = props;
+
+  if (!name) {
+    return { success: false, message: '이름을 올바르게 입력해 주세요.' };
+  } else if (!location) {
+    return { success: false, message: '위치를 올바르게 입력해 주세요.' };
+  } else if (price === undefined || price < 0) {
+    return { success: false, message: '올바른 가격을 입력해 주세요.' };
+  } else if (score === undefined || score < 0 || score > 5) {
+    return { success: false, message: '올바른 평점을 입력해 주세요.' };
+  } else if (!review) {
+    return { success: false, message: '한 줄 평을 입력해 주세요.' };
+  } else if (!amenities) {
+    return { success: false, message: '편의시설을 선택해 주세요.' };
+  }
+
+  return { success: true, message: '' };
+};
