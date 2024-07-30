abfe549c94f1774f923436ac595383a330d4c027
ohgus <123801385+ohgus@users.noreply.github.com>
2024-07-30 12:19:43 +0900

    [remove]: HomeTitle 컴포넌트 제거 (#27)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 'use client';
 
-import Link from 'next/link';
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 
 import Header from '@/components/Header';
 import SearchBar from '@/components/SearchBar';
 import HomePlaceCard from '@/components/HomePlaceCard';
 import db from './db';
-import { AuthContext } from './AuthContext';
 import { PlaceDetailProps } from '@/types/place';
 
 export default function Home() {
@@ -42,7 +40,11 @@ export default function Home() {
     <div>
       <Header />
       <section className="flex justify-center mt-10">
-        <HomeTitle />
+        <div className="mb-6">
+          <h1 className="font-bold text-xl">
+            나만의 여행지를 공유하고, 가고싶은 여행지를 찾아보세요.
+          </h1>
+        </div>
       </section>
       <SearchBar />
       <section className="mt-10 w-8/12 mx-auto">
@@ -61,40 +63,3 @@ export default function Home() {
     </div>
   );
 }
-
-const HomeTitle = () => {
-  const { user } = useContext(AuthContext);
-  const uploadLink = user ? '/upload' : '/login';
-  const myJourneyListLink = user ? '/' : '/login';
-
-  const handleClickLink = () => {
-    if (!user) {
-      alert('로그인 후 이용해주세요.');
-    }
-  };
-  return (
-    <section>
-      <div className="mb-6">
-        <h1 className="font-bold text-xl">
-          나만의 여행지를 공유하고, 가고싶은 여행지를 찾아보세요.
-        </h1>
-      </div>
-      <div className="flex justify-between mb-6">
-        <Link
-          className="font-semibold"
-          href={uploadLink}
-          onClick={handleClickLink}
-        >
-          추천 여행지 등록하기
-        </Link>
-        <Link
-          className="font-semibold"
-          href={myJourneyListLink}
-          onClick={handleClickLink}
-        >
-          내가 추천한 여행지
-        </Link>
-      </div>
-    </section>
-  );
-};
