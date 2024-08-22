840cef1988a7c986fb73279c8e5c3860471fb3f3
ohgus <123801385+ohgus@users.noreply.github.com>
2024-08-22 15:57:59 +0900

    [refactor]: 불필요한 useCallback 제거 (#45)

--- src/hooks/useFetchPlaces.ts
+++ src/hooks/useFetchPlaces.ts
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import {
   collection,
   getDocs,
@@ -20,48 +20,45 @@ export const useFetchPlaces = (contentsPerPage: number) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const fetchPlaces = useCallback(
-    async (page: number) => {
-      if (isLoading) return;
+  const fetchPlaces = async (page: number) => {
+    if (isLoading) return;
 
-      setIsLoading(true);
+    setIsLoading(true);
 
-      try {
-        let placesQuery = query(
-          collection(db, 'places'),
-          orderBy('score', 'desc'),
+    try {
+      let placesQuery = query(
+        collection(db, 'places'),
+        orderBy('score', 'desc'),
+        limit(contentsPerPage)
+      );
+
+      if (lastDoc && page > 1) {
+        placesQuery = query(
+          placesQuery,
+          startAfter(lastDoc),
           limit(contentsPerPage)
         );
-
-        if (lastDoc && page > 1) {
-          placesQuery = query(
-            placesQuery,
-            startAfter(lastDoc),
-            limit(contentsPerPage)
-          );
-        }
-
-        const querySnapshot = await getDocs(placesQuery);
-        let fetchedPlaces = querySnapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        })) as PlaceDetailProps[];
-
-        setPlaces(fetchedPlaces);
-
-        if (querySnapshot.docs.length < contentsPerPage) {
-          setLastDoc(null);
-        } else {
-          setLastDoc(querySnapshot.docs[querySnapshot.docs.length - 1]);
-        }
-      } catch (e) {
-        setError('장소 정보를 가져오는데 실패했습니다. 다시 시도해주세요.');
-      } finally {
-        setIsLoading(false);
       }
-    },
-    [lastDoc, contentsPerPage, isLoading]
-  );
+
+      const querySnapshot = await getDocs(placesQuery);
+      let fetchedPlaces = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      })) as PlaceDetailProps[];
+
+      setPlaces(fetchedPlaces);
+
+      if (querySnapshot.docs.length < contentsPerPage) {
+        setLastDoc(null);
+      } else {
+        setLastDoc(querySnapshot.docs[querySnapshot.docs.length - 1]);
+      }
+    } catch (e) {
+      setError('장소 정보를 가져오는데 실패했습니다. 다시 시도해주세요.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchPlaces(currentPage);
--- src/hooks/useFetchReviews.ts
+++ src/hooks/useFetchReviews.ts
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import {
   collection,
   getDocs,
@@ -21,61 +21,54 @@ export const useFetchReviews = (placeId: string, contentPerPage: number) => {
   const [lastDoc, setLastDoc] = useState<QueryDocumentSnapshot | null>(null);
   const [error, setError] = useState<string | null>(null);
 
-  const fetchReviews = useCallback(
-    async (page: number) => {
-      if (isLoading) return;
+  const fetchReviews = async (page: number) => {
+    if (isLoading) return;
 
-      setIsLoading(true);
+    setIsLoading(true);
 
-      try {
-        let reviewsQuery = query(
+    try {
+      let reviewsQuery = query(
+        collection(db, 'reviews'),
+        where('place_id', '==', placeId),
+        orderBy('date', 'asc'),
+        limit(contentPerPage)
+      );
+
+      if (lastDoc && page > 1) {
+        reviewsQuery = query(
           collection(db, 'reviews'),
           where('place_id', '==', placeId),
           orderBy('date', 'asc'),
+          startAfter(lastDoc),
           limit(contentPerPage)
         );
-
-        if (lastDoc && page > 1) {
-          reviewsQuery = query(
-            collection(db, 'reviews'),
-            where('place_id', '==', placeId),
-            orderBy('date', 'asc'),
-            startAfter(lastDoc),
-            limit(contentPerPage)
-          );
-        }
-
-        const querySnapshot = await getDocs(reviewsQuery);
-        const reviewData = querySnapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        })) as Review[];
-
-        setReviews(reviewData);
-
-        if (querySnapshot.docs.length < contentPerPage) {
-          setLastDoc(null);
-        } else {
-          setLastDoc(querySnapshot.docs[querySnapshot.docs.length - 1]);
-        }
-      } catch (error) {
-        setError('리뷰 정보를 가져오는데 실패했습니다. 다시 시도해주세요.');
-      } finally {
-        setIsLoading(false);
       }
-    },
-    [placeId, contentPerPage, lastDoc, isLoading]
-  );
+
+      const querySnapshot = await getDocs(reviewsQuery);
+      const reviewData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      })) as Review[];
+
+      setReviews(reviewData);
+
+      if (querySnapshot.docs.length < contentPerPage) {
+        setLastDoc(null);
+      } else {
+        setLastDoc(querySnapshot.docs[querySnapshot.docs.length - 1]);
+      }
+    } catch (error) {
+      setError('리뷰 정보를 가져오는데 실패했습니다. 다시 시도해주세요.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchReviews(currentPage);
   }, [currentPage]);
 
-  const paginate = (pageNumber: number) => {
-    if (pageNumber !== currentPage) {
-      setCurrentPage(pageNumber);
-    }
-  };
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return { reviews, currentPage, paginate, error };
 };
