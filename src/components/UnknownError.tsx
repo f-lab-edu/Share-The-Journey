4971130a1aab0a77b99ec660814d7e87e907576a
ohgus <123801385+ohgus@users.noreply.github.com>
2024-08-27 17:05:45 +0900

    [refactor]: 에러 메세지 수정 (#55)

--- src/components/UnknownError.tsx
+++ src/components/UnknownError.tsx
@@ -17,11 +17,22 @@ const UnknownError = ({ onClick, useAt }: UnknownErrorProps) => {
     }
   };
 
+  const getErrorMessage = () => {
+    switch (useAt) {
+      case 'login':
+        return '로그인 중 오류가 발생했습니다.';
+      case 'signUp':
+        return '회원가입 중 오류가 발생했습니다.';
+      case 'upload':
+        return '업로드 중 오류가 발생했습니다.';
+    }
+  };
+
   return (
     <div className="w-2/5 mx-auto flex flex-col bg-yellow-100 p-4 mt-32 rounded-lg">
       <h1 className="mx-auto font-bold text-red-600 text-xl">오류</h1>
       <p className="mx-auto my-5 text-center">
-        알 수 없는 오류가 발생했습니다.
+        {getErrorMessage()}
         <br /> 다시 시도해 주세요.
       </p>
       <Button
