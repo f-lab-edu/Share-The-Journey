954d44ceabaeef208915615ba9f7fdaf80a95aba
ohgus <123801385+ohgus@users.noreply.github.com>
2024-08-27 15:32:51 +0900

    [feat]: 로그인 페이지 예외처리 구현 (#55)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,18 +5,28 @@ import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 
-import Header from '@/components/Header';
-
 import auth from '@/app/auth';
+import { validateEmail, validatePassword } from '@/utils/validate';
 
 const Page = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<
+    'invalid' | 'unknown' | 'password' | 'email' | ''
+  >('');
   const router = useRouter();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError('email');
+      return;
+    } else if (validatePassword(password)) {
+      setError('password');
+      return;
+    }
 
     if (isLoading) return;
 
@@ -27,26 +37,61 @@ const Page = () => {
         router.push('/');
       })
       .catch((err) => {
-        console.error(err);
+        console.error(err.message);
+        if (err.message.includes('invalid-credential')) {
+          setError('invalid');
+        } else {
+          setError('unknown');
+        }
       })
       .finally(() => {
         setIsLoading(false);
       });
   };
 
+  if (error === 'unknown') {
+    return (
+      <div className="w-2/5 mx-auto flex flex-col bg-yellow-100 p-4 mt-32 rounded-lg">
+        <h1 className="mx-auto font-bold text-red-600 text-xl">오류</h1>
+        <p className="mx-auto my-5 text-center">
+          알 수 없는 오류가 발생했습니다.
+          <br /> 다시 시도해 주세요.
+        </p>
+        <Button
+          type="button"
+          color="danger"
+          onClick={() => {
+            setError('');
+          }}
+          className="font-semibold w-3/5 mx-auto"
+        >
+          로그인 페이지로 이동하기
+        </Button>
+      </div>
+    );
+  }
+
   return (
     <>
-      <Header />
       <h1 className="w-2/4 mx-auto text-center font-bold text-2xl mb-5 mt-32">
         로그인
       </h1>
       <div className="w-2/5 mx-auto rounded-lg bg-slate-100 p-4">
         <form className="flex flex-wrap" onSubmit={handleSubmit}>
           <div className="mb-3 font-semibold w-full">
+            {error === 'invalid' && (
+              <p className="font-semibold text-center text-red-600">
+                이메일 또는 비밀번호를 확인해주세요.
+              </p>
+            )}
             <Input
               className="bg-white rounded-xl"
+              isInvalid={error === 'email' || error === 'invalid'}
+              errorMessage={
+                error === 'email' ? '이메일 형식이 올바르지 않습니다.' : ''
+              }
               variant="bordered"
-              type="email"
+              type="text"
               name="email"
               label="이메일"
               labelPlacement="outside"
@@ -58,6 +103,10 @@ const Page = () => {
           <div className="mb-5 font-semibold w-full">
             <Input
               variant="bordered"
+              isInvalid={error === 'password' || error === 'invalid'}
+              errorMessage={
+                error === 'password' ? '비밀번호는 6자 이상이어야 합니다.' : ''
+              }
               label="비밀번호"
               labelPlacement="outside"
               placeholder="비밀번호를 입력해주세요."
