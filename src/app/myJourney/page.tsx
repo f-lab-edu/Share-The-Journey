64a0825d7bb77809d198af7674ae72fde6f97856
ohgus <123801385+ohgus@users.noreply.github.com>
2024-08-28 10:43:11 +0900

    [feat]: 마이 페이지 레이아웃 구현 (#56)

---
+++ src/app/myJourney/page.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useState, useContext, useEffect } from 'react';
+import { Button } from '@nextui-org/react';
+
+import MyPlaceCard from '@/components/MyPlaceCard';
+import { useFetchMyPlaces } from '@/hooks/useFetchMyPlaces';
+import { AuthContext } from '@/app/AuthContext';
+
+const Page = () => {
+  const { user } = useContext(AuthContext);
+  const [uid, setUid] = useState<string | null>(null);
+  const contentPerPage = 9;
+
+  useEffect(() => {
+    if (user?.uid) {
+      setUid(user.uid);
+    }
+  }, [user]);
+
+  const { places, error, currentPage, moveToNextPage, moveToPrevPage } =
+    useFetchMyPlaces(contentPerPage, uid || '');
+
+  if (!user) return <div>로딩중...</div>;
+
+  return (
+    <div className="w-8/12 mx-auto pl-3">
+      <h1 className="text-xl font-semibold mt-10 text-start mb-5">
+        내가 등록한 여행지 {places.length}개
+      </h1>
+      <div className="grid grid-cols-3 gap-10">
+        {places.map((place) => (
+          <div key={place.id}>
+            <MyPlaceCard {...place} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
