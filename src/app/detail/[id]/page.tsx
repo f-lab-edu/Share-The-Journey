1bb043f568633550ec164be591b6a891c1ae6b95
ohgus <123801385+ohgus@users.noreply.github.com>
2024-09-09 18:59:41 +0900

    [refactor]: edit, detail 페이지 컴포넌트 수정 (#66) - 클라이언트 컴포넌트로 변경 - 로딩, 에러 컨텐츠 추가

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,23 +1,43 @@
+'use client';
+
+import { Spinner } from '@nextui-org/react';
+
 import ReviewArea from '@/components/ReviewArea';
 import ImgCarousel from '@/components/ImgCarousel';
 import PlaceInfo from '@/components/PlaceInfo';
 import { useFetchPlace } from '@/hooks/useFetchPlace';
 
-const Page = async ({ params }: { params: { id: string } }) => {
-  const place = await useFetchPlace(params.id);
+const Page = ({ params }: { params: { id: string } }) => {
+  const { placeData, placeError, isLoading } = useFetchPlace(params.id);
 
-  if (!place) {
+  if (isLoading) {
     return (
-      <div className="flex items-center justify-center h-screen text-lg font-bold text-slate-600">
-        해당 장소가 존재하지 않습니다.
+      <div className="flex items-center justify-center h-screen">
+        <Spinner color="default" />
+      </div>
+    );
+  }
+
+  if (placeError) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p>{placeError}</p>
+      </div>
+    );
+  }
+
+  if (!placeData) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p>해당 장소가 존재하지 않습니다.</p>
       </div>
     );
   }
 
   return (
     <>
-      <ImgCarousel imgUrls={place.imgUrls} />
-      <PlaceInfo {...place} />
+      <ImgCarousel imgUrls={placeData.imgUrls} />
+      <PlaceInfo {...placeData} />
       <ReviewArea placeId={params.id} />
     </>
   );
--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,15 +1,25 @@
+'use client';
+
 import { Spinner } from '@nextui-org/react';
 
 import PlaceForm from '@/components/PlaceForm';
 import { useFetchPlace } from '@/hooks/useFetchPlace';
 
-const Page = async ({ params }: { params: { id: string } }) => {
-  const placeData = await useFetchPlace(params.id);
+const Page = ({ params }: { params: { id: string } }) => {
+  const { placeData, placeError, isLoading } = useFetchPlace(params.id);
 
-  if (!placeData) {
+  if (isLoading) {
     return (
       <div className="flex items-center justify-center h-screen">
         <Spinner color="default" />
+      </div>
+    );
+  }
+
+  if (placeError) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p>{placeError}</p>
       </div>
     );
   }
@@ -19,7 +29,11 @@ const Page = async ({ params }: { params: { id: string } }) => {
       <h1 className="mt-20 text-center font-bold text-2xl mb-5">
         나의 여행지 수정하기
       </h1>
-      <PlaceForm initialData={placeData} mode={'edit'} id={params.id} />
+      <PlaceForm
+        initialData={placeData ?? undefined}
+        mode={'edit'}
+        id={params.id}
+      />
     </div>
   );
 };
